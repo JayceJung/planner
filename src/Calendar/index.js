4d302fbfa5b970a4cfd09e4fca230fa0e1ff60d7
JayceJung <jaycejung0@gmail.com>
2019-09-08 14:27:53 -0400

    Add Calendar

---
+++ src/Calendar/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+import { ToDoList } from './todo';
+import './calendar.css';
+
+export default class Test extends Component {
+  state = {
+    value: new Date(),
+  };
+
+  onChange = value => this.setState({ value });
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div>
+        <Calendar onChange={this.onChange} value={value} calendarType="US" />
+        <ToDoList value={value} />
+      </div>
+    );
+  }
+}
